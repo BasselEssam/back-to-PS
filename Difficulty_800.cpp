d1f24bf34443ee58edc9804da7e272448925f271
Bassel Essam <101820549+BasselEssam@users.noreply.github.com>
2024-07-18 20:33:53 +0300

    Update Difficulty_800.cpp
    (A) Way Too Long Words

--- Difficulty_800.cpp
+++ Difficulty_800.cpp
@@ -1,10 +1,11 @@
+                                          // A. Bit++
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
-                                            // A. Bit++
+                                            
     int n=0;
     int x=0;
     string st="";
@@ -18,6 +19,31 @@ int main()
                 x--;
         }
     cout<<x;
+
+    return 0;
+}
+
+                                            // A. Way Too Long Words
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    while(n>0)
+        {
+           string str;
+           cin>>str;
+           if(str.length()>10)
+            {
+                cout<<str[0]<<str.length()-2<<str[str.length()-1]<<endl;
+            }
+           else
+            cout<<str<<endl;
+           n--;
+        }
 
     return 0;
 }
